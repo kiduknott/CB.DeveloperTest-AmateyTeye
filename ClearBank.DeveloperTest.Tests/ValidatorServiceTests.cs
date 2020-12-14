dbf4d4f7431f523052aae22f2243736f45991a39
kiduknott <amatey_teye@yahoo.co.uk>
2020-12-14 19:45:42 +0000

    Added ValidatorService tests for Bacs

--- ClearBank.DeveloperTest.Tests/ValidatorServiceTests.cs
+++ ClearBank.DeveloperTest.Tests/ValidatorServiceTests.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AutoFixture;
 using AutoFixture.NUnit3;
 using ClearBank.DeveloperTest.Services;
 using ClearBank.DeveloperTest.Types;
@@ -17,19 +14,44 @@ namespace ClearBank.DeveloperTest.Tests
         [InlineAutoData(PaymentScheme.Bacs)]
         [InlineAutoData(PaymentScheme.FasterPayments)]
         [InlineAutoData(PaymentScheme.Chaps)]
-        public void Null_Account_Returns_False_For_All_Payment_Types(PaymentScheme scheme, MakePaymentRequest paymentRequest)
+        public void Null_Account_Returns_False_For_All_Payment_Types(PaymentScheme scheme, MakePaymentRequest request)
         {
             //arrange
             Account account = null;
-            paymentRequest.PaymentScheme = scheme;
+            request.PaymentScheme = scheme;
 
             var sut = new ValidatorService();
 
             //act
-            var result = sut.ValidatePayment(account, paymentRequest);
+            var result = sut.ValidatePayment(account, request);
             
             //assert
             result.Should().BeEquivalentTo(new MakePaymentResult {Success = false});
+        }
+
+        [Test]
+        [InlineAutoData(AccountStatus.Live, "99999999999.99")]
+        [InlineAutoData(AccountStatus.Live, "0.01")]
+        [InlineAutoData(AccountStatus.Live, "0.00")]
+        [InlineAutoData(AccountStatus.Live, "-0.01")]
+        [InlineAutoData(AccountStatus.Disabled, "0.01")]
+        [InlineAutoData(AccountStatus.InboundPaymentsOnly, "0.01")]
+        public void Account_With_Bacs_Allowed_Always_Returns_False_For_Bacs_Payments(AccountStatus accountStatus,
+            string accountBalance, Account account, MakePaymentRequest request)
+        {
+            //arrange
+            account.AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs;
+            account.Status = accountStatus;
+            account.Balance = Convert.ToDecimal(accountBalance);
+            request.PaymentScheme = PaymentScheme.Bacs;
+
+            var sut = new ValidatorService();
+
+            //act
+            var result = sut.ValidatePayment(account, request);
+
+            //assert
+            result.Should().BeEquivalentTo(new MakePaymentResult { Success = true });
         }
     }
 }
--- ClearBank.DeveloperTest/Services/ValidatorService.cs
+++ ClearBank.DeveloperTest/Services/ValidatorService.cs
@@ -7,13 +7,46 @@ namespace ClearBank.DeveloperTest.Services
 {
     public class ValidatorService : IValidatorService
     {
-        public MakePaymentResult ValidatePayment(Account account, MakePaymentRequest paymentRequest)
+        public MakePaymentResult ValidatePayment(Account account, MakePaymentRequest request)
         {
             var result = new MakePaymentResult();
-            
+
             if (null == account)
             {
                 result.Success = false;
+                return result;
+            }
+            
+            switch (request.PaymentScheme)
+            {
+                case PaymentScheme.Bacs:
+                    if (account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Bacs))
+                    {
+                        result.Success = true;
+                    }
+                    break;
+
+                case PaymentScheme.FasterPayments:
+                    if (account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments))
+                    {
+                        result.Success = true;
+                    }
+                    else if (account.Balance > request.Amount)
+                    {
+                        result.Success = true;
+                    }
+                    break;
+
+                case PaymentScheme.Chaps:
+                    if (account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps))
+                    {
+                        result.Success = true;
+                    }
+                    else if (account.Status == AccountStatus.Live)
+                    {
+                        result.Success = true;
+                    }
+                    break;
             }
 
             return result;
